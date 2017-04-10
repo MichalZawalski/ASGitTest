57706f177e257b4d667086f0c376ac69a122422f
Michał Zawalski <michaael250@gmail.com>
2017-04-10 15:30:08 +0200

    sth

--- app/src/main/java/com/example/myfirstapp/DisplayMessageActivity.java
+++ app/src/main/java/com/example/myfirstapp/DisplayMessageActivity.java
@@ -1,11 +1,16 @@
 package com.example.myfirstapp;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
+    private StatusControl status;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +23,31 @@ public class DisplayMessageActivity extends AppCompatActivity {
 
         // Capture the layout's TextView and set the string as its text
         TextView textView = (TextView) findViewById(R.id.textView);
-        textView.setText("Witaj " + message);
 
+        textView.setText("Witaj " + MainActivity.getLogin());
+
+        status = StatusControl.getInstance();
+        status.displayStatus((TextView) findViewById(R.id.statusField));
+    }
+
+    public void statusList(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(DisplayMessageActivity.this);
+
+        builder.setItems(status.getStatusList(), new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                status.setStatus(which);
+                status.displayStatus((TextView) findViewById(R.id.statusField));
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
+        return;
+    }
+
+    public void openSettings(View view) {
+        Intent intent = new Intent(this, Settings.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -8,6 +8,8 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+
+    private static String login;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +22,11 @@ public class MainActivity extends AppCompatActivity {
         EditText editText = (EditText) findViewById(R.id.editText);
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
+        login = message;
         startActivity(intent);
+    }
+
+    public static String getLogin() {
+        return login;
     }
 }
